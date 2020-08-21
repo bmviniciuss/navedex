8db08b1cdb57ef5a1b8560f60f481a087536b41a
Vinicius Barbosa <bmvinicius11@gmail.com>
2020-08-21 10:40:50 -0300

    feat: add Input error state

--- src/compoents/Input/index.tsx
+++ src/compoents/Input/index.tsx
@@ -5,11 +5,12 @@ type LabelProps = {
     text: string
     htmlFor: string
   }
+  error: string | undefined
 }
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & LabelProps
 
-const Input:React.FC<Props> = ({ label, ...props }:Props) => {
+const Input:React.FC<Props> = ({ label, error, ...props }:Props) => {
   const inputClass = useMemo(() => {
     const baseCss = 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
     let classes = baseCss
@@ -17,8 +18,12 @@ const Input:React.FC<Props> = ({ label, ...props }:Props) => {
       classes = `${baseCss} bg-gray-200 cursor-not-allowed`
     }
 
+    if (error) {
+      classes = `${baseCss} mb-3 border-red-500`
+    }
+
     return classes
-  }, [props.disabled])
+  }, [props.disabled, error])
 
   return (
     <div className="mt-5">
@@ -26,6 +31,7 @@ const Input:React.FC<Props> = ({ label, ...props }:Props) => {
         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={label.htmlFor}>{label.text}</label>
       )}
       <input className={inputClass} {...props} />
+      {error && <p className="text-red-500 text-xs italic">{error}</p>}
     </div>
   )
 }
