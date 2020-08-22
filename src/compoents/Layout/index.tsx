0f849fdd55c66975462ea9b8ef05e2c58796e6e2
Vinicius Barbosa <bmvinicius11@gmail.com>
2020-08-22 15:48:37 -0300

    feat: new bg color

--- src/compoents/Layout/index.tsx
+++ src/compoents/Layout/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Layout:React.FC = ({ children }) => {
   return (
-    <div className="bg-gray-200 h-full w-full">
+    <div className="bg-gray-100 h-full w-full">
       <nav className="p-4 border">Navigation TODO</nav>
       <main className="mx-auto mt-8" style={{ width: '90%', maxWidth: '1280px' }}>{children}</main>
     </div>
