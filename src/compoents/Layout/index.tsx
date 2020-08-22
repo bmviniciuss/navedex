21346e31b73c32cf60e4dc91300484dab2355a6b
Vinicius Barbosa <bmvinicius11@gmail.com>
2020-08-22 15:15:23 -0300

    refactor: add container to main layout section

--- src/compoents/Layout/index.tsx
+++ src/compoents/Layout/index.tsx
@@ -4,7 +4,7 @@ const Layout:React.FC = ({ children }) => {
   return (
     <div className="bg-gray-200 h-full w-full">
       <nav className="p-4 border">Navigation TODO</nav>
-      <main className="p-4">{children}</main>
+      <main className="mx-auto mt-8" style={{ width: '90%', maxWidth: '1280px' }}>{children}</main>
     </div>
   )
 }
