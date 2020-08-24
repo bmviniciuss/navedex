96a8ccda47e2641a3ed3c23754d9343b719d6f01
Vinicius Barbosa <bmvinicius11@gmail.com>
2020-08-24 10:32:47 -0300

    feat: add Footer

--- src/compoents/Layout/index.tsx
+++ src/compoents/Layout/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Footer from '../Footer'
 import Navbar from '../Navbar'
 
 const Layout:React.FC = ({ children }) => {
@@ -7,6 +8,7 @@ const Layout:React.FC = ({ children }) => {
     <div className="bg-gray-100 h-full w-full text-gray-900 antialiased">
       <Navbar />
       <main className="mx-auto mt-8" style={{ width: '90%', maxWidth: '1280px' }}>{children}</main>
+      <Footer />
     </div>
   )
 }
