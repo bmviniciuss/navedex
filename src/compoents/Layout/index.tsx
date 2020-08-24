7aeba8b07d2ccdd82cf66e6677dc5b1abc5351b3
Vinicius Barbosa <bmvinicius11@gmail.com>
2020-08-23 22:33:30 -0300

    feat: add text gray-900 as default color and antialiased on layout

--- src/compoents/Layout/index.tsx
+++ src/compoents/Layout/index.tsx
@@ -4,7 +4,7 @@ import Navbar from '../Navbar'
 
 const Layout:React.FC = ({ children }) => {
   return (
-    <div className="bg-gray-100 h-full w-full">
+    <div className="bg-gray-100 h-full w-full text-gray-900 antialiased">
       <Navbar />
       <main className="mx-auto mt-8" style={{ width: '90%', maxWidth: '1280px' }}>{children}</main>
     </div>
