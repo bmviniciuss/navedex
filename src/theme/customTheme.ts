a6792d9798db04ea50fc321de534d8e22c727ac6
Vinicius Barbosa <bmvinicius11@gmail.com>
2020-08-24 11:48:57 -0300

    refactor: use tailwind colors on chakra

--- src/theme/customTheme.ts
+++ src/theme/customTheme.ts
@@ -8,6 +8,10 @@ export const customTheme:DefaultTheme = {
   ...chakraTheme,
   colors: {
     ...chakraTheme.colors,
-    blue: { ...chakraColors.blue, ...tailwind.theme.colors.blue }
+    blue: { ...chakraColors.blue, ...tailwind.theme.colors.blue },
+    green: { ...chakraColors.green, ...tailwind.theme.colors.green },
+    gray: { ...chakraColors.gray, ...tailwind.theme.colors.gray },
+    red: { ...chakraColors.red, ...tailwind.theme.colors.red },
+    orange: { ...chakraColors.orange, ...tailwind.theme.colors.orange }
   }
 }
