2881bea90461bbf984f8e324fd2440d058884cc3
Vinicius Barbosa <bmvinicius11@gmail.com>
2020-08-25 09:11:16 -0300

    refactor: remove naver modal title

--- src/components/NaverModal/index.tsx
+++ src/components/NaverModal/index.tsx
@@ -1,14 +1,14 @@
-import { IconButton } from '@chakra-ui/core'
+import { Button } from '@chakra-ui/core'
 import moment from 'moment'
 import React, { useContext } from 'react'
 import { MdModeEdit, MdDelete } from 'react-icons/md'
 import { useQuery } from 'react-query'
 import { useHistory } from 'react-router-dom'
-import { HashLoader } from 'react-spinners'
 
 import NaverModalContext from '../../contexts/NaverModalContext'
 import { getNaver } from '../../external/api'
 import { Naver } from '../../types'
+import HashLoader from '../HashLoader'
 import Modal from '../Modal'
 
 import 'moment/locale/pt-br'
@@ -31,15 +31,10 @@ const NaverModal:React.FC<Props> = ({ naver, onClose }) => {
   })
 
   return (
-    <Modal isOpen={true} title="Naver" onClose={onClose} size={700} footer={<div></div>}>
-      {isLoading && (
-        <div className="flex flex-col justify-center items-center h-full w-full my-10">
-          <HashLoader size="35px" css='display: block; margin: 0 auto;' color="#1A202C" loading={isLoading} />
-        </div>
-      )
-      }
+    <Modal isOpen={true} title="" onClose={onClose} size={700} footer={<div></div>}>
+      {isLoading && (<HashLoader className="my-10" loading={isLoading} />)}
       {!isLoading && data && (
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-8">
           <div>
             <div className="h-full w-full">
               <img className="h-full w-full rounded-md shadow-lg" src={data.url} alt={data.url}/>
@@ -56,32 +51,36 @@ const NaverModal:React.FC<Props> = ({ naver, onClose }) => {
               <p className="text-gray-900">{moment().diff(moment.utc(data.birthdate), 'years')} Anos</p>
 
               <p className="text-sm mt-2 text-gray-600">Tempo de Empresa</p>
-              <p className="text-gray-900">{moment.utc(data.admission_date).fromNow()}</p>
+              <p className="text-gray-900">{moment.utc(data.admission_date).fromNow(true)}</p>
 
               <p className="text-sm mt-2 text-gray-600">Projeto</p>
               <p className="text-gray-900">{data.project}</p>
             </div>
 
             <div className="mt-4">
-              <IconButton
+              <Button
                 className="focus:outline-none text-gray-900 mr-2"
-                aria-label="Remover"
-                icon={MdDelete}
-                onClick={() => {
-                  openDeleteNaver(data)
-                  onClose()
-                }}
-              />
-
-              <IconButton
-                className="focus:outline-none text-gray-900"
-                aria-label="Atualizar"
-                icon={MdModeEdit}
+                aria-label="editar naver"
+                leftIcon={MdModeEdit}
                 onClick={() => {
                   onClose()
                   history.push(`/navers/editar/${data.id}`)
                 }}
-              />
+              >
+                Editar
+              </Button>
+
+              <Button
+                className="focus:outline-none text-gray-900 "
+                aria-label="Remover"
+                leftIcon={MdDelete}
+                onClick={() => {
+                  openDeleteNaver(data)
+                  onClose()
+                }}
+              >
+                Remover
+              </Button>
             </div>
           </div>
 
