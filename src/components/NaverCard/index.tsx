e42002704e6d323d323ddd37251b3d4b3bcb6b07
Vinicius Barbosa <bmvinicius11@gmail.com>
2020-08-25 09:00:38 -0300

    refactor: remove age from NaverCard

--- src/components/NaverCard/index.tsx
+++ src/components/NaverCard/index.tsx
@@ -23,21 +23,16 @@ const NaverCard:React.FC<Props> = ({ naver }) => {
       <div className="relative" style={{ paddingBottom: '100%' }}>
         <img className="absolute h-full w-full object-cover rounded-lg shadow-md" src={naver.url} alt={naver.name} />
       </div>
-      <div className="relative px-4 -mt-4">
-        <div className="bg-white p-6 rounded-lg shadow-lg h-full">
-          <div className="flex items-baseline flex-wrap">
+      <div className="relative px-1 -mt-6">
+        <div className="bg-white p-6 rounded-lg shadow-lg">
+          <div className="flex">
             <span className="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-sm uppercase font-semibold tracking-wide break-words truncate">
               {naver.job_role}
             </span>
-            <div className="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide break-words">
-              {age}
-            </div>
           </div>
-          <h4 className="mt-1 font-semibold text-lg leading-tight break-words">{naver.name}</h4>
-          <div className="mt-2">
-            <div className="text-gray-600 text-sm"> Projetos</div>
-            { naver.project ? naver.project : 'Sem Projetos'}
-          </div>
+          <h4 className="mt-1 font-semibold text-lg leading-tight break-words truncate">{naver.name}</h4>
+          <p className="mt-3 truncate text-gray-600 text-sm">Projetos</p>
+          <p className="truncate">{ naver.project ? naver.project : 'Sem Projetos'}</p>
           <div className="mt-4">
             <Button className="text-gray-900 w-full focus:outline-none" onClick={() => openNaver(naver)}>Mostrar</Button>
             <div className="mt-2 grid grid-cols-2 gap-4">
