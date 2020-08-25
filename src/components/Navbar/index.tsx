2b7a573461b0c185df851d955535b5acc0a0a0f1
Vinicius Barbosa <bmvinicius11@gmail.com>
2020-08-25 08:09:34 -0300

    fix: navbar breakpoints

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ const Navbar:React.FC = () => {
   return (
     <header className="text-gray-700 py-1 body-font border">
       <div className="mx-auto flex flex-wrap flex-col md:flex-row items-center" style={{ width: '90%', maxWidth: '1280px' }}>
-        <Link to="/navers" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <Link to="/navers" className="flex title-font font-medium items-center text-gray-900 md:mb-0">
           <Logo className="w-24"/>
         </Link>
         <div className="md:ml-auto flex flex-wrap items-center text-base justify-center">
@@ -22,7 +22,7 @@ const Navbar:React.FC = () => {
                 logout()
                 history.push('/')
               }}
-              className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0"
+              className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base md:mt-0"
             >
             Sair
               <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
