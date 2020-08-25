7e231514c075b5d04dd41717bef58f02bc72cd8f
Vinicius Barbosa <bmvinicius11@gmail.com>
2020-08-25 09:20:53 -0300

    refactor: remove age variables in NaverCard

--- src/components/NaverCard/index.tsx
+++ src/components/NaverCard/index.tsx
@@ -1,6 +1,5 @@
 import { Button, IconButton } from '@chakra-ui/core'
-import moment from 'moment'
-import React, { useContext, memo, useMemo } from 'react'
+import React, { useContext, memo } from 'react'
 import { MdModeEdit, MdDelete } from 'react-icons/md'
 import { useHistory } from 'react-router-dom'
 
@@ -14,9 +13,6 @@ interface Props {
 const NaverCard:React.FC<Props> = ({ naver }) => {
   const { openNaver, openDeleteNaver } = useContext(NaverModalContext)
   const history = useHistory()
-  const age = useMemo(() => {
-    return moment.utc(naver.birthdate).fromNow(true)
-  }, [naver.birthdate])
 
   return (
     <div >
